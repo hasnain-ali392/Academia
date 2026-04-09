edd970a5db5f6b869542e2ddb8a25d8c211537d8
Hasnainali404 <sanwarali990@gmail.com>
2026-04-09 21:27:29 +0500

    chore: untrack files excluded by .gitignore

--- .backup_src/lib/constants.ts
+++
@@ -1,6 +0,0 @@
-export const NAV_LINKS = [
-  { label: "Home", href: "/" },
-  { label: "About", href: "/about" },
-  { label: "Calculator", href: "/calculator" },
-  { label: "Contact", href: "/contact" },
-];
